12e74e45cbaa7a3db7b11421e93685731fae26e4
jklypchak13 <jarod.klypchak@yahoo.com>
2019-01-16 20:20:25 -0500

    stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,21 @@ let canvas = document.getElementById("c");
 let missles = [];
 //let person = new Person();
 let background = new Image();
-//background.src = "filename";
+let counter = 0;
+background.src = "img/background.png";
 setup();
 
 function setup() {
     setInterval(main, 10);
-    missles.push(new Missle(0, 50, 10, 10, 1, null, 1, 0));
 }
 
+let limit = 100;
 
 function main() {
     let ctx = canvas.getContext("2d");
-    //ctx.drawImage(this.image, 400, 144);
-    ctx.fillStyle = "black";
-    console.log(missles[0]);
-    ctx.fillRect(0, 0, 400, 144);
+    ctx.drawImage(background, 0, 0);
+    //ctx.fillStyle = "black";
+    //ctx.fillRect(0, 0, 400, 144);
     for (let i = 0; i < missles.length; i++) {
         //if (person.hit(missles[i])) {
         //    console.log("HIT, GAME OVER");
@@ -24,8 +24,21 @@ function main() {
         missles[i].update();
         missles[i].display();
     }
+    counter++;
+    if (counter == limit) {
+        createNewMissle();
+        counter = 0;
+        limit -= 1;
+    }
+
 }
 
 function createNewMissle() {
-    let x = Math.floor(Math.random() * 144);
+    let x = Math.floor(Math.random() * 400);
+    let y = Math.floor(Math.random() * 2) + 1
+    if (y == 1) {
+        missles.push(new Missle(x, 0, 20, 20, 1, null, 0, 1));
+    } else {
+        missles.push(new Missle(x, 144, 20, 20, 1, null, 0, -1));
+    }
 }
--- scripts/missle.js
+++ scripts/missle.js
@@ -5,19 +5,21 @@ class Missle {
         this.width = w;
         this.height = h;
         this.speed = speed;
-        this.xVelocity = 1;
-        this.yVelocity = 0;
+        this.xVelocity = vx;
+        this.yVelocity = vy;
         this.target = target;
         this.image = new Image();
-        //this.image.src = "image/person???";
+        this.image.width = w;
+        this.image.height = h;
+        this.image.src = "img/meteor.png";
         //this.calculateVelocity();
     }
 
     display() {
         var ctx = canvas.getContext("2d");
-        //ctx.drawImage(this.image, this.width, this.height);
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        // ctx.fillStyle = "red";
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
     update() {
